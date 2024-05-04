5ab6c6d47c190088f8c73ad8b93b1c8d2d825685
aman <amansingh2002may18@gmail.com>
2024-05-04 17:04:46 +0530

    "Props Basic"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Child from './Components/Props/Props1/Child'
+import Card from './Components/Props/Props2/Card'
+import "./App.css";
 
 const App = () => {
+
   return (
     <div>
-      <Child userName="Aman_Singh" city="Noida"></Child>
+      <Child userName="Pushkar" city="Noida"></Child>
+      <Card></Card>
     </div>
   )
 }
-
 export default App

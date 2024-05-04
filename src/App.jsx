742b00b533214e387571c3836a1ddb15dbd567ce
aman <amansingh2002may18@gmail.com>
2024-05-04 16:43:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Child from './Components/Props/Props1/Child'
+
+const App = () => {
+  return (
+    <div>
+      <Child userName="Aman_Singh" city="Noida"></Child>
+    </div>
+  )
+}
+
+export default App
